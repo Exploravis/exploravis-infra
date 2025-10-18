0d615985858de96f64c18037999e3e666eee29d6
x0rw <mahdi.svt5@gmail.com>
2025-10-18 16:48:48 +0100

    add dynamic dns assignment to cluster master

---
+++ scripts/get_master_ip.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$(pwd)"
+TF_DIR="$ROOT_DIR/terraform"
+PY_SCRIPT="$ROOT_DIR/scripts/get_master_ip.py"
+
+terraform -chdir="$TF_DIR" output -json | python3 "$PY_SCRIPT"
