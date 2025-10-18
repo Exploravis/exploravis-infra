b35a97a7c89aa558e3496a415106dfeefaa79b91
x0rw <mahdi.svt5@gmail.com>
2025-10-18 15:54:11 +0100

    Install and setup Teleport through Ansible

--- scripts/run_ansible_inv.sh
+++ scripts/run_ansible_inv.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 ROOT_DIR="$(pwd)"
 INVENTORY_DIR="$ROOT_DIR/ansible/inventories"
 PLAYBOOK="$ROOT_DIR/ansible/playbooks/playbook.yml"
+PLAYBOOK_2="$ROOT_DIR/ansible/playbooks/teleport.yml"
 ANSIBLE_CFG="$ROOT_DIR/ansible/ansible.cfg"
 
 export ANSIBLE_CONFIG="$ANSIBLE_CFG"
@@ -15,8 +16,9 @@ echo "----------------------------------------"
 shopt -s nullglob
 for inventory_file in "$INVENTORY_DIR"/*; do
   if [[ -f "$inventory_file" ]]; then
-    echo "Running playbook on inventory: $inventory_file"
+    echo "Running playbook on inventory: $inventory_file "
     ansible-playbook -i "$inventory_file" "$PLAYBOOK"
+    # ansible-playbook -i "$inventory_file" "$PLAYBOOK_2"
     echo "----------------------------------------"
   fi
 done
--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -11,18 +11,18 @@ locals {
   inventory_dir = "${path.module}/artifacts"
 }
 
-resource "local_file" "ansible_inventory_per_cluster" {
-  for_each = module.clusters
-  filename = "${local.inventory_dir}/${each.key}_inventory.ini"
-
-  content = <<EOT
-[master]
-${each.value.master.name} ansible_host=${each.value.master.public_ip} ansible_user=${var.ansible_user} ansible_ssh_private_key_file=${var.ansible_ssh_private_key_file}
-
-[workers]
-%{for w in each.value.workers~}
-${w.name} ansible_host=${w.public_ip} ansible_user=${var.ansible_user} ansible_ssh_private_key_file=${var.ansible_ssh_private_key_file}
-%{endfor~}
-EOT
-}
-
+# resource "local_file" "ansible_inventory_per_cluster" {
+#   for_each = module.clusters
+#   filename = "${local.inventory_dir}/${each.key}_inventory.ini"
+#
+#   content = <<EOT
+# [master]
+# ${each.value.master.name} ansible_host=${each.value.master.public_ip} ansible_user=${var.ansible_user} ansible_ssh_private_key_file=${var.ansible_ssh_private_key_file}
+#
+# [workers]
+# %{for w in each.value.workers~}
+# ${w.name} ansible_host=${w.public_ip} ansible_user=${var.ansible_user} ansible_ssh_private_key_file=${var.ansible_ssh_private_key_file}
+# %{endfor~}
+# EOT
+# }
+#
